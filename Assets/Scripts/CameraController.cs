f12560c98c487117543544a87fc1fcc51c1a406a
Jordan Wilson <jordan@smallboxweb.com>
2015-12-02 10:01:40 -0500

    added a camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// private references
+	private GameObject player;
+
+	// vectors
+	private Vector3 velocity = Vector3.zero;
+
+	// floats
+	public float verticalOffset = 0;
+	private float smoothTime = 20f;
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void FixedUpdate()
+	{
+		// *its recommended to use LateUpdate() for camera following, but I'm getting some stuttering with its Time.deltaTime value
+
+		// move towards the player's position
+		Vector3 target = player.transform.TransformPoint(new Vector3(0, verticalOffset, -20));
+		transform.position = Vector3.Lerp(transform.position, target, smoothTime * Time.deltaTime);
+
+		// move towards the player's rotation
+		transform.rotation = Quaternion.Slerp(transform.rotation, player.transform.rotation, smoothTime * Time.deltaTime);
+	}
+}
--- Assets/Scripts/MinimapController.cs
+++ Assets/Scripts/MinimapController.cs
@@ -6,11 +6,23 @@ public class MinimapController : MonoBehaviour {
 	// private references
 	private GameObject player;
 	private GameObject planet;
+	private GameObject backgroundPlanet;
+
+	// vectors
+	private Vector2 backgroundPlanetSize;
 
 	void Start()
 	{
 		player = GameObject.FindGameObjectWithTag("Player");
 		planet = transform.Find("Planet").gameObject;
+
+		backgroundPlanet = GameObject.Find("Planet Background").gameObject;
+		backgroundPlanetSize = backgroundPlanet.GetComponent<SpriteRenderer>().sprite.rect.size;
+		//Debug.LogFormat("{0}, {1}, {2}", backgroundPlanetSize, Screen.width, Screen.height);
+
+		//float camHalfHeight = Camera.main.orthographicSize;
+		//float camHalfWidth = Camera.main.aspect * camHalfHeight; 
+		//Debug.LogFormat("{0}, {1}", camHalfHeight, camHalfWidth);
 	}
 	
 	void Update()
@@ -20,9 +32,30 @@ public class MinimapController : MonoBehaviour {
 
 	void AlignTopRight()
 	{
+		/*
 		Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width - 75, Screen.height - 75, 0));
 		transform.position = new Vector2(pos.x, pos.y);
 		//transform.rotation = Camera.main.transform.rotation;
+		*/
+
+		/** /
+		pos = Camera.main.ScreenToWorldPoint(new Vector3(backgroundPlanetSize.x, Screen.height - backgroundPlanetSize.y, 0));
+		backgroundPlanet.transform.position = new Vector3(pos.x, pos.y, 1);
+		backgroundPlanet.transform.rotation = Camera.main.transform.rotation;
+		/**/
+
+		/*
+		float camHalfHeight = Camera.main.orthographicSize;
+		float camHalfWidth = Camera.main.aspect * camHalfHeight; 
+		Bounds bounds = backgroundPlanet.GetComponent<SpriteRenderer>().bounds;
+		
+		// Set a new vector to the top left of the scene 
+		Vector3 topLeftPosition = new Vector3(-camHalfWidth, camHalfHeight, 0) + Camera.main.transform.position; 
+		
+		// Offset it by the size of the object 
+		topLeftPosition += new Vector3(bounds.size.x / 2,-bounds.size.y / 2, 0);
+		backgroundPlanet.transform.position = topLeftPosition; 
+		*/
 	}
 
 }
